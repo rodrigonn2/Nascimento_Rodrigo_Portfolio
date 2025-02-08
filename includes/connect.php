314e3d736d57f9f1772371ad7686b97dd123cad6
rodrigonn2 <144962992+rodrigonn2@users.noreply.github.com>
2025-02-07 19:53:27 -0500

    PDO UPDATING

--- includes/connect.php
+++ includes/connect.php
@@ -1,4 +1,11 @@
 <?php
-//Creates a connection to the database. This code is 'included' into another file, as if it is pasted into the other file.
-$connect = new mysqli('localhost','root','root','nascimento_rodrigo_portfolio');
+//$connection = new mysqli('localhost','root','root','nascimento_rodrigo_portfolio');//
+$dsn = "mysql:host=localhost;dbname=nascimento_rodrigo_portfolio;charset=utf8mb4";
+try {
+$connection = new PDO($dsn, 'root', 'root');
+} catch (Exception $e) {
+  error_log($e->getMessage());
+  exit('unable to connect');
+}
+
 ?>
